b5fc5312114344d1d19bd9e19759e098f34042a3
Mackenzie Wilson <mackenzie.w115@gmail.com>
2018-03-12 07:35:00 -0400

    Working on adding some particle and sound effects

--- Assets/Scripts/EvolutionManager.cs
+++ Assets/Scripts/EvolutionManager.cs
@@ -16,6 +16,11 @@ public class EvolutionManager : MonoBehaviour {
     [HideInInspector] public int m_chaserEvolveCount = 0;
     [HideInInspector] public Transform m_chaserLastPos;
 
+    [SerializeField] private GameObject m_gEvolutionParticle;
+    private float m_evolutionTimer = 2f;
+
+    private bool m_isEvolveParticle = false;
+
     void Update ()
     {
         // Evolve Bouncer Into Chaser type enemy
@@ -23,15 +28,28 @@ public class EvolutionManager : MonoBehaviour {
         {
             foreach (Collider2D item in m_bouncersInRange)
             {
-                if (item.CompareTag("Enemy0"))
+                if (item.CompareTag("Enemy0") && m_evolutionTimer <= 0f)
                 {
                     Destroy(item.gameObject);
                 }
             }
-            Instantiate(m_chaserEnemy, m_bouncerLastPos.position, m_bouncerLastPos.rotation);
-            m_bouncerEvolveCount = 0;
-            m_bouncerLastPos = null;
-            m_bouncersInRange.Clear();
+            m_evolutionTimer -= Time.deltaTime;
+            GameObject evolutionParticle = null;
+            if (!m_isEvolveParticle)
+            {
+                evolutionParticle = Instantiate(m_gEvolutionParticle, m_bouncerLastPos.position, m_bouncerLastPos.rotation);
+                m_isEvolveParticle = true;
+            }
+            if (m_evolutionTimer <= 0f)
+            {
+                Instantiate(m_chaserEnemy, m_bouncerLastPos.position, m_bouncerLastPos.rotation);
+                m_bouncerLastPos = null;
+                Destroy(evolutionParticle);
+                m_isEvolveParticle = false;
+                m_bouncersInRange.Clear();
+                m_evolutionTimer = 2f;
+                m_bouncerEvolveCount = 0;
+            }
         }
 
         // Evolve Chaser Into Dasher type enemy
@@ -50,4 +68,6 @@ public class EvolutionManager : MonoBehaviour {
             m_chasersInRange.Clear();
         }
     }
+
+
 }
